d751f649646d4bf9b75d9eee613cec85dcf5a8f6
Zhenya <manixjen@gmail.com>
2023-01-13 23:43:55 +0300

    add ZippingFileReader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,11 +12,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         /*Crypto.encrypt("abc","src/TestingFiles/input.txt","src/TestingFiles/input.txt");
-        Crypto.encrypt("bca","src/TestingFiles/input.txt","src/TestingFiles/input.txt");*/
+        Crypto.encrypt("bca","src/TestingFiles/input.txt","src/TestingFiles/input.txt");
+        ArchivingFileManager.zippingFile("src/TestingFiles/input.txt");*/
 
         BuilderFileReaderImpl builderFileReader = new BuilderFileReaderImpl("src/TestingFiles/input.txt",Txt);
         builderFileReader.setEncrypting("abc");
         builderFileReader.setEncrypting("bca");
+        builderFileReader.setZipping();
         IFileReader fileReader = builderFileReader.build();
         System.out.println(fileReader.read());
     }
