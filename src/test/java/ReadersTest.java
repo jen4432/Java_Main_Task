4891705b930d13b0c415e9a527d16d50ca7ce24d
Zhenya <manixjen@gmail.com>
2023-01-15 10:40:33 +0300

    add TxtFileReaderTest

---
+++ src/test/java/ReadersTest.java
@@ -0,0 +1,34 @@
+import Readers.TxtFileReader;
+import Readers.XmlFileReader;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReadersTest {
+    ArrayList<String> expected = new ArrayList<>(Arrays.asList("3-4+2","100-10","20/0"));
+
+    @Test
+    public void TxtFileReaderTest(){
+        TxtFileReader txtFileReader = new TxtFileReader("src/TestingFiles/TestInputTxt.txt");
+        try {
+            ArrayList<String> actual = txtFileReader.read();
+            assertEquals(expected,actual);
+
+        } catch (Exception e) {
+            System.out.println("Error at TxtFileReaderTest : " + e.toString());
+        }
+    }
+
+    @Test
+    public void XmlFileReadeTest(){
+        XmlFileReader xmlFileReader = new XmlFileReader("src/TestingFiles/TestInputXml.xml");
+        ArrayList<String> actual = xmlFileReader.read();
+        System.out.println(actual);
+        System.out.println(expected);
+        assertEquals(expected,actual);
+    }
+
+}
