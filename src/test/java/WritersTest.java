e6f333476ea4ad7e87a13d30b9175382492e59db
Zhenya <manixjen@gmail.com>
2023-01-15 13:24:35 +0300

    add TxtFileWriterTest

--- src/test/java/WritersTest.java
+++ src/test/java/WritersTest.java
@@ -1,6 +1,21 @@
+import Readers.TxtFileReader;
+import Writers.TxtFileWriter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class WritersTest {
-    ArrayList<String> expected = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> expected = new ArrayList<String>(Arrays.asList("0.0","12","14"));
+
+    @Test
+    public void TxtWriteTest() throws IOException {
+        TxtFileWriter txtFileWriter = new TxtFileWriter("src/TestingFiles/TestOutputTxt.txt");
+        txtFileWriter.write(expected);
+        TxtFileReader txtFileReader = new TxtFileReader("src/TestingFiles/TestOutputTxt.txt");
+        ArrayList<String> actual = txtFileReader.read();
+        assertEquals(expected,actual);
+    }
 }
